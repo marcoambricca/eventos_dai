9ddfd2cbce439c28a0d94745d2475fa097e18e65
Android Studio <devandroid@ort>
2024-06-24 17:01:16 -0300

    common repo, service

--- src/repositories/common-repository.js
+++ src/repositories/common-repository.js
@@ -3,5 +3,52 @@ import pkg from 'pg';
 const { Client } = pkg;
 
 export default class CommonRepository{
-    
+    getAllSync = async (table_name) => {
+        let returnArray = null;
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = `SELECT * FROM ${table_name}`;
+            const result = await client.query(sql);
+            await client.end();
+            returnArray = result.rows;
+        }
+        catch (error){
+            console.log(error);
+        }
+        return returnArray;
+    }
+    getByIdSync = async (table_name, id) => {
+        let returnObject = null;
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = `SELECT * FROM ${table_name} WHERE id = $1`;
+            const values = [id];
+            const result = await client.query(sql, values);
+            await client.end();
+            returnObject = result.rows[0];
+        }
+        catch (error){
+            console.log(error);
+        }
+        return returnObject;
+    }
+    deleteByIdAsync = async (table_name, id) => {
+        let rowsAffected = 0;
+        const client = new Client(DBConfig);
+        try {
+            await client.connect();
+            const sql = `DELETE FROM ${table_name} WHERE id = $1`;
+            const values = [id];
+            const result = await client.query(sql, values);
+            await client.end();
+
+            rowsAffected = result.rowCount;
+        }
+        catch (error){
+            console.log(error);
+        }
+        return rowsAffected;
+    }
 }
--- src/services/common-service.js
+++ src/services/common-service.js
@@ -1,1 +1,21 @@
 import CommonRepository from "../repositories/common-repository.js";
+
+export default class CommonService{
+    getAllSync = async (table_name) => {
+        const repo = new CommonRepository();
+        let returnArray = await repo.getAllSync();
+        return returnArray;
+    }
+    
+    getByIdSync = async (table_name, id) => {
+        const repo = new CommonRepository();
+        let returnObject = await repo.getByIdSync(table_name, id);
+        return returnObject;
+    }
+    
+    deleteByIdAsync = async (table_name, id) => {
+        const repo = new CommonRepository();
+        let response = await repo.deleteByIdAsync(table_name, id);
+        return response;
+    }
+}
